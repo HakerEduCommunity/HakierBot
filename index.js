24b86c15faea13b8ea31d89d7609d04539927ffa
Kirito <kacper.potyrala@gmail.com>
2018-02-26 20:10:52 +0100

    Komenda kick - do naprawy :construction_worker:

--- index.js
+++ index.js
@@ -3,37 +3,48 @@ const client = new Discord.Client()
 // Please replace with your own token :D
 const token = require('./token.js')
 const config = {
-    prefix: '!'
-  }
+  prefix: '!'
+}
 
 client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setActivity('Hakuje Pentagon')
-  });
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity('Hakuje Pentagon')
+});
 
-  client.on("guildMemberAdd", function(member) {
-      member.guild.channels.find("name", "powitalnia").send(member.toString() + ", Witamy na Haker.Edu.PL! Miłego dnia  :wink: !");
-    });
+client.on("guildMemberAdd", function (member) {
+  member.guild.channels.find("name", "powitalnia").send(member.toString() + ", Witamy na HakerEduPL! Miłego dnia  :wink: !");
+});
 
-  client.on("guildMemberAdd", member => {
-    console.log('Użytkownik' + " " + member.username + ' has joined the server!')
+client.on("guildMemberAdd", member => {
+  var role = member.guild.roles.find('name', 'Użytkownik');
 
-    var role = member.guild.roles.find('name', 'Użytkownik');
+  member.addRole(role)
+});
 
-    member.addRole(role)
-  });
+client.on('message', async (message) => {
+  const command = message.content.slice(config.prefix.length)
 
-  client.on('message', async (message) => {
-    const command = message.content.slice(config.prefix.length)
-  
-    if (message.author.bot) return
-  
-    if (message.content.indexOf(config.prefix) !== 0) return
+  if (message.author.bot) return
 
-    if(command === 'ping') {
-        await message.channel.send(new Date().getTime() - message.createdTimestamp + " ms");        
-      }
+  if (message.content.indexOf(config.prefix) !== 0) return
 
-    })
+  if (command === 'ping') {
+    await message.channel.send(new Date().getTime() - message.createdTimestamp + " ms");
+  }
 
-    client.login(token)
+  if(command === "kick") {
+    if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.reply("sorry, you don't have permission to kick members.");
+    if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) return message.reply("it seems I do not have permission to perform this action. Does my role have the kick membes permission?");
+    let userToKick = message.mentions.users.first();
+
+    if( message.mentions.users.size < 1) return message.reply("you did not provide a user to kick. Aborting operation.");
+    if(!message.guild.member(userToKick)
+    .kickable) return message.reply("that user has a role above my highest role.")
+
+    await member.kick(reason)
+          .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+        message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+  }
+});
+
+client.login(token)
