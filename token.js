85a6ccf0dd2f4d8f6e541c47645062f340722281
Unknown <kacper.potyrala@gmail.com>
2018-02-27 19:08:55 +0100

    Token

--- token.js
+++ token.js
@@ -1,2 +1,2 @@
-const token = 'Mzk1MjQwMzk5NzUwMjk5NjU4.DSQBKQ.qlQwEZXJZGFnJJ3nI8_IaRH7dVs'
+const token = 'Mzk1MjQwMzk5NzUwMjk5NjU4.DSQBKQ.qlQwEZXJZGFnJJ3nI8_IaRH7dVs' // testowy token 
 module.exports = token
