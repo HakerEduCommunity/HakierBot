dc59f20358380b90551f61d66c305718a98f1237
takidelfin <takidelfin@gmail.com>
2018-03-28 22:34:20 +0200

    :rotating_light: Fixed `kick.js`

--- src/commands/kick.js
+++ src/commands/kick.js
@@ -1,38 +1,35 @@
 const Discord = module.require('discord.js')
 
 module.exports.run = async (bot, message, args) => {
+  if (message.guild.member(bot.user).hasPermission('KICK_MEMBERS')) {
+    if (!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send(':warning: You have no permission to kick members!')
 
-  if(message.guild.member(bot.user).hasPermission('KICK_MEMBERS')) {
+    if (args.length < 2) {
+      return message.channel.send('**[Admin Mode]** Too few arguments! Type cody_help kick to get usage of this command.')
+    }
 
-    if(!message.member.hasPermission('KICK_MEMBERS')) return message.channel.send(':warning: You have no permission to kick members!')
+    if (args.length >= 2) {
+      const toBan = message.mentions.members.first() || message.guild.members.get(args[0])
 
-      if(args.length < 2) {
-        return message.channel.send(`**[Admin Mode]** Too few arguments! Type cody_help kick to get usage of this command.`)
+      if (!toBan) return message.channel.send(':warning: You did not specify a valid user mention or ID!')
+
+      const reason = args.slice(1)
+
+      if (!toBan.user.bot) {
+        await toBan.user.send(`**You have been kicked from ${message.guild.name}. Reason: ${reason.join(' ')}**`)
       }
 
-      if(args.length >= 2) {
-        let toBan = message.mentions.members.first() || message.guild.members.get(args[0])
-
-        if (!toBan) return message.channel.send(':warning: You did not specify a valid user mention or ID!')
-
-        let reason = args.slice(1)
-
-        if(!toBan.user.bot) {
-          await toBan.user.send(`**You have been kicked from ${message.guild.name}. Reason: ${reason.join(' ')}**`)
-        }
-        
-        let embed = new Discord.RichEmbed()
+      const embed = new Discord.RichEmbed()
         .setAuthor(`${toBan.user.username}#${toBan.user.discriminator} was kicked`, toBan.user.displayAvatarURL)
         .setDescription(`**Kicked by**: ${message.author}\n**Reason**: ${reason.join(' ')}`)
-        message.channel.send(embed)
-        await message.guild.member(toBan).kick()
-      }
-
-    } else {
-      return message.channel.send(`:warning: I have no permission to kick users!`)
+      message.channel.send(embed)
+      await message.guild.member(toBan).kick()
     }
+  } else {
+    return message.channel.send(':warning: I have no permission to kick users!')
+  }
 }
 
 module.exports.help = {
-  name: 'kick'
+  name: 'kick',
 }
