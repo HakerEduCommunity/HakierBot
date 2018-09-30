d8049990dafe87e840e31e0edbf6e50d967452f7
SerekKiri <kacper.potyrala@gmail.com>
2018-09-30 12:36:48 +0200

    fix: Fixed null file spamming

--- src/commands/virustotal.js
+++ src/commands/virustotal.js
@@ -1,19 +1,19 @@
+const VirusTotalToken = '995e4b11fcbe66a9c8bcbc9c6df5459f394bf2575999bb5d67c1929d2e13019f'
+const request = require('request')
+const fs = require('fs')
+
 function virustotal(message) {
   try {
-    const VirusTotalToken = '995e4b11fcbe66a9c8bcbc9c6df5459f394bf2575999bb5d67c1929d2e13019f'
-    const request = require('request')
-    const fs = require('fs')
-
-    const StatusMessage = message.channel.send("Preparing...")
-    var Attachment = (message.attachments).array()
-    const downloadLink = Attachment[0].url //link to download file from discord
+    const StatusMessage = message.channel.send('Preparing...')
+    const Attachment = (message.attachments).array()
+    const downloadLink = Attachment[0].url // link to download file from discord
     console.log(downloadLink)
 
-    const regex = /[^/]{0,}$/gm;
-    const name = downloadLink.match(regex); //name of file
+    const regex = /[^/]{0,}$/gm
+    const name = downloadLink.match(regex) // name of file
 
     // Download file to scan
-    StatusMessage.then(sentMessage => sentMessage.edit("Downloading..."));
+    StatusMessage.then(sentMessage => sentMessage.edit('Downloading...'))
     const download = request.get(downloadLink)
     download.on('response', (res) => {
       res.pipe(fs.createWriteStream(`${__dirname}/VirusTotalFiles/plik.exe`))
@@ -21,7 +21,7 @@ function virustotal(message) {
         console.log('pobrano plik!')
 
         // Upload File to VirusTotal
-        StatusMessage.then(sentMessage => sentMessage.edit("Upload to VirusTotal..."));
+        StatusMessage.then(sentMessage => sentMessage.edit('Uploaded to VirusTotal...'))
         const formData = {
           apikey: VirusTotalToken,
           file: fs.createReadStream(`${__dirname}/VirusTotalFiles/plik.exe`),
@@ -30,14 +30,17 @@ function virustotal(message) {
           if (err) {
             return console.error('upload failed:', err)
           }
-          
-          if (JSON.parse(body).md5 === 'd41d8cd98f00b204e9800998ecf8427e') { console.log('NULLFILE') }
+
+          if (JSON.parse(body).md5 === 'd41d8cd98f00b204e9800998ecf8427e') {
+            console.log('NULLFILE')
+            return message.reply('Dlaczego mi wysyłasz pusty plik? ...')
+          }
           const VTRaportLink = JSON.parse(body).permalink
           console.log(VTRaportLink)
-        
-        
+
+
           // GET Raport about File from VirusTotal
-          StatusMessage.then(sentMessage => sentMessage.edit("Wait for Raport from VirusTotal..."));
+          StatusMessage.then(sentMessage => sentMessage.edit('Wait for Raport from VirusTotal...'))
           const formData = {
             apikey: VirusTotalToken,
             resource: JSON.parse(body).resource,
@@ -45,61 +48,65 @@ function virustotal(message) {
           let IsScanedSuccessfull = false
           // Checking Loop
           const xyz = (function checkRaport() {
-            if(IsScanedSuccessfull==false) {
+            if (IsScanedSuccessfull === false) {
               console.log('sprawdzam')
 
               request.post({ url: 'https://www.virustotal.com/vtapi/v2/file/report', formData }, (err, _httpResponse, body) => {
                 if (err) {
                   return console.error('Check Status failed:', err)
                 }
-                //console.log(body);
-                const VTReportRespone = JSON.parse(body)
-                if (VTReportRespone.md5 === 'd41d8cd98f00b204e9800998ecf8427e') {
-                  console.log('NULL FILE!')
-                } else if (VTReportRespone.response_code == -2) {
-                  console.log('wciaz sprawdzam')
-                } else if (VTReportRespone.response_code == 1) {
-                  console.log('WYJDZ MAM TO CO CHCE')
-                  IsScanedSuccessfull = true
-                  console.log(`${VTReportRespone.positives}/${VTReportRespone.total}`)
+                // console.log(body);
+                try {
+                  const VTReportRespone = JSON.parse(body)
+                  if (VTReportRespone.md5 === 'd41d8cd98f00b204e9800998ecf8427e') {
+                    console.log('NULL FILE!')
+                  } else if (VTReportRespone.response_code === -2) {
+                    console.log('wciaz sprawdzam')
+                  } else if (VTReportRespone.response_code === 1) {
+                    console.log('WYJDZ MAM TO CO CHCE')
+                    IsScanedSuccessfull = true
+                    console.log(`${VTReportRespone.positives}/${VTReportRespone.total}`)
 
-                  VTmessage(StatusMessage, VTRaportLink, VTReportRespone.positives, VTReportRespone.total)
-                } else {
-                  console.log(body)
+                    VTmessage(StatusMessage, VTRaportLink, VTReportRespone.positives, VTReportRespone.total)
+                  } else {
+                    console.log(body)
+                  }
+                } catch (err) {
+                  return console.log(err)
                 }
               })
-              setTimeout(checkRaport, 5000);
+              setTimeout(checkRaport, 5000)
             }
-          })();
+          })
         })
       })
     })
-  } catch (err) {
-    console.log(console.error())
-  }
-}
 
-function VTmessage(tracker, link, positives, total) {
-  const embed = {
-    title: `File scaned (${positives}/${total})`,
-    description: `[Link to Analysis](${link})`,
-    color: '22684',
-    timestamp: '2018-08-11T13:35:04.860Z',
-    footer: {
-      icon_url: 'https://raw.githubusercontent.com/HakerEduCommunity/design-assets/master/assets/discord-thumbnail.png',
-      text: 'Hakier Bot by takidelfin and Kiritito',
-    },
-    thumbnail: {
-      url: 'https://static-dot-virustotalcloud.appspot.com/ui-public/images/thumbprint.png',
-    },
-    author: {
-      name: 'VirusTotal',
-      url: link,
-      icon_url: 'https://static-dot-virustotalcloud.appspot.com/ui-public/images/thumbprint.png',
-    },
+    function VTmessage(tracker, link, positives, total) {
+      const embed = {
+        title: `File scaned (${positives}/${total})`,
+        description: `[Link to Analysis](${link})`,
+        color: '22684',
+        timestamp: '2018-08-11T13:35:04.860Z',
+        footer: {
+          icon_url: 'https://raw.githubusercontent.com/HakerEduCommunity/design-assets/master/assets/discord-thumbnail.png',
+          text: 'Hakier Bot by takidelfin and Kiritito',
+        },
+        thumbnail: {
+          url: 'https://static-dot-virustotalcloud.appspot.com/ui-public/images/thumbprint.png',
+        },
+        author: {
+          name: 'VirusTotal',
+          url: link,
+          icon_url: 'https://static-dot-virustotalcloud.appspot.com/ui-public/images/thumbprint.png',
+        },
+      }
+      // message.channel.send({ embed })
+      tracker.then(sentMessage => sentMessage.edit({ embed }))
+    }
+  } catch (err) {
+    console.log(err)
   }
-  //message.channel.send({ embed })
-  tracker.then(sentMessage => sentMessage.edit({ embed }));
 }
 
 module.exports = virustotal
--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const client = new Discord.Client({ disableEveryone: true })
 
 client.on('ready', () => {
   console.log(`\nInitialized on ${new Date().toUTCString()}.`)
-  client.user.setPresence({ game: { name: 'Haker.edu.pl', type: 3 } });
+  client.user.setPresence({ game: { name: 'Haker.edu.pl', type: 3 } })
 })
 
 client.on('guildMemberAdd', async (member) => {
@@ -29,54 +29,54 @@ client.commands = new Discord.Collection()
 
 client.on('message', async (message) => {
   // Is File attachment (VirusTotal)
-  if (message.attachments.first(1).length > 0){
+  if (message.attachments.first(1).length > 0) {
     virustotal(message)
   }
 
   // const command = message.content.startsWith(config.prefix)
   const check = commands.includes(message.content.slice(1))
 
-  if (message.content.startsWith(config.prefix + 'c++') && check === true) {
+  if (message.content.startsWith(`${config.prefix}c++`) && check === true) {
     roles(message)
   }
 
-  if (message.content.startsWith(config.prefix + 'python') && check === true) {
+  if (message.content.startsWith(`${config.prefix}python`) && check === true) {
     roles(message)
   }
 
-  if (message.content.startsWith(config.prefix + 'c#') && check === true) {
+  if (message.content.startsWith(`${config.prefix}c#`) && check === true) {
     roles(message)
   }
 
-  if (message.content.startsWith(config.prefix + 'lua') && check === true) {
+  if (message.content.startsWith(`${config.prefix}lua`) && check === true) {
     roles(message)
   }
 
-  if (message.content.startsWith(config.prefix + 'html') && check === true) {
+  if (message.content.startsWith(`${config.prefix}html`) && check === true) {
     roles(message)
   }
 
-  if (message.content.startsWith(config.prefix + 'java') && check === true) {
+  if (message.content.startsWith(`${config.prefix}java`) && check === true) {
     roles(message)
   }
 
-  if (message.content.startsWith(config.prefix + 'php') && check === true) {
+  if (message.content.startsWith(`${config.prefix}php`) && check === true) {
     roles(message)
   }
 
-  if (message.content.startsWith(config.prefix + 'swift') && check === true) {
+  if (message.content.startsWith(`${config.prefix}swift`) && check === true) {
     roles(message)
   }
 
-  if (message.content.startsWith(config.prefix + 'golang') && check === true) {
+  if (message.content.startsWith(`${config.prefix}golang`) && check === true) {
     roles(message)
   }
 
-  if (message.content.startsWith(config.prefix + 'visual-basic') && check === true) {
+  if (message.content.startsWith(`${config.prefix}visual-basic`) && check === true) {
     roles(message)
   }
 
-  if (message.content.startsWith(config.prefix + 'help') && check === true) {
+  if (message.content.startsWith(`${config.prefix}help`) && check === true) {
     help(message)
   }
 
